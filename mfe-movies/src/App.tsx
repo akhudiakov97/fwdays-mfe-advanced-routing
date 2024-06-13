d58387e2742a9e92d8b68de22b322949afa7f37d
akhudiak <next.teacher@lectrum.io>
2024-06-13 16:25:12 +0100

    code based routing

--- mfe-movies/src/App.tsx
+++ mfe-movies/src/App.tsx
@@ -1,32 +1,62 @@
 import {
+  Outlet,
   RouterProvider,
   createMemoryHistory,
+  createRootRoute,
+  createRoute,
   createRouter,
 } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
-import { routeTree } from "./routeTree.gen";
 import { createContext } from "react";
+import About from "./components/About";
+import Content from "./components/Content";
 
 const memoryHistory = createMemoryHistory({
   initialEntries: ["/"], // Pass your initial url
 });
 
-// Create a new router instance
-const router = createRouter({ routeTree, history: memoryHistory });
 
-// Register the router instance for type safety
-declare module "@tanstack/react-router" {
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: Content,
+});
+
+const aboutRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/about",
+  component: About,
+});
+
+
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute])
+
+const router = createRouter({ routeTree, history: memoryHistory  })
+
+
+declare module '@tanstack/react-router' {
   interface Register {
-    router: typeof router;
+    router: typeof router
   }
 }
 
+
 type Props = {
-  onNavigate: (to: string) => void;
+  onNavigate?: (to: string) => void;
 };
 
 interface NavigationContextProps {
-  onNavigate: (to: string) => void;
+  onNavigate?: (to: string) => void;
 }
 
 export const NavigationContext = createContext<
